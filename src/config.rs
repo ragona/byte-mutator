3e980e94e5cc568a8c4ef4fbff036879809f8e55
ragona <ryanragona@gmail.com>
2019-10-28 21:23:56 -0700

    parse config file

--- src/config.rs
+++ src/config.rs
@@ -1,9 +1,27 @@
 use serde_derive::Deserialize;
+use std::fs;
+use std::io::{self, Error};
+use std::path::Path;
 use toml;
 
 #[derive(Deserialize, Debug)]
-struct Config {
+pub struct Config {
     seeds: Vec<SeedConfig>,
+}
+
+impl Config {
+    pub fn from_file(path: &str) -> std::io::Result<Config> {
+        match fs::read_to_string(path) {
+            Ok(s) => match toml::from_str(&s) {
+                Ok(c) => Ok(c),
+                Err(e) => Err(Error::new(
+                    io::ErrorKind::InvalidInput,
+                    "Failed to parse config",
+                )),
+            },
+            Err(e) => Err(e),
+        }
+    }
 }
 
 #[derive(Deserialize, Debug)]
@@ -16,7 +34,7 @@ struct SeedConfig {
 struct MutationConfig {
     name: String,
     iterations: u32,
-    range: [u32; 2],
+    range: Option<[u32; 2]>,
 }
 
 #[cfg(test)]
@@ -24,7 +42,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn foo() {
+    fn config_from_string() {
         let config: Config = toml::from_str(
             r#"
             # a list of seeds to start from
